454b9aa3091ab31d3c8f4a2f1131553bf0abeef6
Devon Hogencamp <devhogen1@gmail.com>
2017-04-19 14:44:45 -0700

    beginning router

---
+++ backend/routes/router.js
@@ -0,0 +1,11 @@
+/*
+    Router - Routes our CRUD API reqs and sends res
+*/
+
+// Express
+const express = require('express');
+
+// Express router
+const router = express.Router();
+
+/* API ROUTES */
