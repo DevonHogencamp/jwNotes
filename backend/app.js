8e40172dddab5b2e40cbc2e5cb29cceda0dd0ba4
Devon Hogencamp <devhogen1@gmail.com>
2017-04-21 11:58:51 -0700

    added the mongoose calls and body parser as middleware

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,26 @@
 // Express object
 const express = require('express');
 
+// Body Parser
+const bodyParser = require('body-parser');
+
+// Mongoose
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/jwNotes');
+
 // Setup express app
 const app = express();
 
+// Set up body parser for express
+app.use(bodyParser.json());
+
+// Use the router with all of its routes with api attached on the front of each req
+app.use('/api', require('./routes/router'));
+
 // Port Number
 const port = 3000;
-
-app.use('/api', require('./routes/router'));
 
 // Setting up the server with a message to tell us everything is up and running
 app.listen(port, function () {
